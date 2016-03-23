33df987b5db0914e8390a8bb05d5c22b05a9075a
Eleni <elenipaskali@yahoo.com>
2016-03-23 20:30:56 +0000

    Menu Controller

---
+++ ChryslerCarnage/Assets/elena/scripts/GoBack.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoBack : MonoBehaviour {
+    public string levelname;
+    public void GoLevel() {
+        Application.LoadLevel(levelname);
+    }
+}
